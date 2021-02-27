9b7ad9664be541d0c5d8afb48f26b172896b0acd
DevanggaCP <devangga1234567@gmail.com>
2021-02-27 09:58:42 +0700

    update frontend

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="row ipad-width">
                 <div class="col-md-8">
                     <div class="title-hd">
-                        <h2>in theater</h2>
+                        <h2>film bioskop</h2>
                         <a href="#" class="viewall">View all <i class="ion-ios-arrow-right"></i></a>
                     </div>
                     <div class="tabs">
@@ -16,7 +16,7 @@
                         </ul>
                         <div class="tab-content">
                             <div id="tab1" class="tab active">
-                                <div class="row">
+                                <div class="row"> 
                                     <div class="slick-multiItem">
                                         <div class="slide-it">
                                             <div class="movie-item">
@@ -150,47 +150,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="sidebar">
-                        <div class="ads">
-                            <img src="images/uploads/ads1.png" alt="" width="336" height="296">
-                        </div>
-                        <div class="celebrities">
-                            <h4 class="sb-title">Spotlight Celebrities</h4>
-                            <div class="celeb-item">
-                                <a href="#"><img src="images/uploads/ava1.jpg" alt="" width="70" height="70"></a>
-                                <div class="celeb-author">
-                                    <h6><a href="#">Samuel N. Jack</a></h6>
-                                    <span>Actor</span>
-                                </div>
-                            </div>
-                            <div class="celeb-item">
-                                <a href="#"><img src="images/uploads/ava2.jpg" alt="" width="70" height="70"></a>
-                                <div class="celeb-author">
-                                    <h6><a href="#">Benjamin Carroll</a></h6>
-                                    <span>Actor</span>
-                                </div>
-                            </div>
-                            <div class="celeb-item">
-                                <a href="#"><img src="images/uploads/ava3.jpg" alt="" width="70" height="70"></a>
-                                <div class="celeb-author">
-                                    <h6><a href="#">Beverly Griffin</a></h6>
-                                    <span>Actor</span>
-                                </div>
-                            </div>
-                            <div class="celeb-item">
-                                <a href="#"><img src="images/uploads/ava4.jpg" alt="" width="70" height="70"></a>
-                                <div class="celeb-author">
-                                    <h6><a href="#">Justin Weaver</a></h6>
-                                    <span>Actor</span>
-                                </div>
-                            </div>
-                            <a href="#" class="btn">See all celebrities<i class="ion-ios-arrow-right"></i></a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
-    @include('layouts/components/frontend/latest')
 @endsection
